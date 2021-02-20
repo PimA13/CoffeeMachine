fe5f03cc61c37d1fc5a9e37168e55cf7005b531d
PimA13 <76967951+PimA13@users.noreply.github.com>
2021-02-20 20:28:25 +0300

    Update CoffeeMachine.java

--- CoffeeMachine.java
+++ CoffeeMachine.java
@@ -1,14 +1,11 @@
 
 public class CoffeeMachine {
 
-
     public static void main(String[] args) {
         Coffee coffeeMachine = new Coffee();
 
         while (coffeeMachine.getPower()) {
             coffeeMachine.selectAction();
         }
-
     }
-
 }
